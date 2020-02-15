d3e6588ad208b71e729b30131762ed71f3723f81
LifeAlertt <60244208+LifeAlertt@users.noreply.github.com>
2020-02-15 16:45:44 -0500

    Created Room System object to organize room layouts

---
+++ GameFiles/Assets/Scripts/Rooms.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rooms : MonoBehaviour
+{
+    // Rooms with South opening
+    public GameObject[] southRooms;
+
+    // Rooms with North opening
+    public GameObject[] northRooms;
+
+    // Rooms with East opening
+    public GameObject[] eastRooms;
+
+    // Rooms with West opening
+    public GameObject[] westRooms;
+}
