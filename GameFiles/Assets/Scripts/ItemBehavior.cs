9d1c958f399fe140ef406f8032b3727fb572efc1
wc6255 <wchen6255@outlook.com>
2020-02-23 21:32:01 -0500

    added inventory and items

---
+++ GameFiles/Assets/Scripts/ItemBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    int id;
+
+    public ItemDatabase itemDB;
+    public Rigidbody2D rbody;
+    public GameObject player;
+    public Inventory inventory;
+
+    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject == player)
+        {
+            inventory.collectItem(id);
+            Destroy(this.gameObject);
+        }
+        Debug.Log("Collided with player");
+    }
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        inventory = player.GetComponent<Inventory>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GameFiles/Assets/Scripts/UIInventory.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIInventory : MonoBehaviour
+{
+    public List<UIItem> uiItems = new List<UIItem>();
+    public GameObject slotPrefab;
+    public Transform slotPanel;
+    public int slotNum = 16; //default number of inventory slots
+
+    private void Awake()
+    {
+        for(int i=0;i<slotNum;i++)
+        {
+            GameObject instance = Instantiate(slotPrefab);
+            instance.transform.SetParent(slotPanel);
+            uiItems.Add(instance.GetComponentInChildren<UIItem>());
+        }
+    }
+
+    public void updateSlot(int slot, Item item)
+    {
+        uiItems[slot].updateItem(item);
+    }
+
+    public void addItem(Item item)
+    {
+        updateSlot(uiItems.FindIndex(i => i.item == null), item);
+    }
+
+    public void removeItem(Item item)
+    {
+        updateSlot(uiItems.FindIndex(i => i.item == item), null);
+    }
+}
