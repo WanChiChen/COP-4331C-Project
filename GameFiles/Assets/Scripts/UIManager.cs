adbd03437dea446d85f6c5e8f8d5b1e6201cd521
wc6255 <wchen6255@outlook.com>
2020-03-02 00:48:02 -0500

    added pausing

--- GameFiles/Assets/Scripts/UIManager.cs
+++ GameFiles/Assets/Scripts/UIManager.cs
@@ -7,6 +7,7 @@ public class UIManager : MonoBehaviour
 {
     private AssetBundle sceneAssetBundle;
     private string[] scenePath;
+    GameObject[] pauseObjects;
 
     //intialize object
     void Start()
@@ -14,6 +15,13 @@ public class UIManager : MonoBehaviour
         sceneAssetBundle = AssetBundle.LoadFromFile("Assets/Scenes");
         scenePath = sceneAssetBundle.GetAllScenePaths();
 
+    }
+
+    private void Awake()
+    {
+        Time.timeScale = 1;
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+        hidePaused();
     }
 
     //Load an input scene
@@ -26,4 +34,55 @@ public class UIManager : MonoBehaviour
     {
         SceneManager.LoadScene(scenePath[0]);
     }
+
+    private void Update()
+    {
+        //uses the p button to pause and unpause the game
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                showPaused();
+            }
+            else if (Time.timeScale == 0)
+            {
+                Debug.Log("high");
+                Time.timeScale = 1;
+                hidePaused();
+            }
+        }
+    }
+    public void pauseControl()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            showPaused();
+        }
+        else if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            hidePaused();
+        }
+    }
+
+    //shows objects with ShowOnPause tag
+    public void showPaused()
+    {
+        foreach (GameObject obj in pauseObjects)
+        {
+            obj.SetActive(true);
+        }
+    }
+
+    //hides objects with ShowOnPause tag
+    public void hidePaused()
+    {
+        foreach (GameObject obj in pauseObjects)
+        {
+            obj.SetActive(false);
+        }
+    }
+
 }
