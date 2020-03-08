57a3a3b2d2685b6dc20f9bcf2864107c5910f59d
LifeAlertt <60244208+LifeAlertt@users.noreply.github.com>
2020-03-08 17:32:56 -0400

    Updated NeuralNetworks to setup nodes

--- GameFiles/Assets/Scripts/NeuralNetwork.cs
+++ GameFiles/Assets/Scripts/NeuralNetwork.cs
@@ -17,12 +17,54 @@ public class NeuralNetwork : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        SetupLayers();
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //Gives nodes access to network, assigns layers
+    void SetupLayers()
+    {
+        SetupInputLayer();
+
+        SetupHiddenLayer();
+
+        SetupOutputLayer();
+    }
+
+    // Setup each node in input layer
+    void SetupInputLayer()
+    {
+        foreach(AINode node in inputLayer)
+        {
+            // Set node as layer 0
+            node.Setup(0);
+        }
+    }
+
+    // Setup each node in hidden layer
+    void SetupHiddenLayer()
+    {
+        foreach (AINode node in hiddenLayer)
+        {
+            // Set node as layer 1
+            node.Setup(1);
+        }
+    }
+
+    // Setup each node in output layer
+    void SetupOutputLayer()
+    {
+        foreach (AINode node in outputLayer)
+        {
+            // Set node as layer 2
+            node.Setup(2);
+        }
     }
 }
