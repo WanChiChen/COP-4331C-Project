48650a9e470d4f5c926756074b727265a4e8b455
LifeAlertt <60244208+LifeAlertt@users.noreply.github.com>
2020-03-08 17:19:52 -0400

    Added fields to Neural Network, added new functions to AINode

--- GameFiles/Assets/Scripts/AINode.cs
+++ GameFiles/Assets/Scripts/AINode.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class AINode : MonoBehaviour
 {
     // Array of weights to each connection
-    public int[] weights;
+    public float[] weights;
 
     // Array of numbers corresponding to connected nodes
     public int[] connections;
@@ -16,8 +16,11 @@ public class AINode : MonoBehaviour
     // Node input (for input layer nodes only)
     private float input;
 
-    // Check flag for if input node
-    private bool isInputLayer;
+    // Layer of this node
+    private int layer;
+
+    // Reference to network
+    private NeuralNetwork network;
 
     // Access output
     public float GetOutput()
@@ -29,24 +32,38 @@ public class AINode : MonoBehaviour
     public void SetInput(float value)
     {
         input = value;
-        isInputLayer = true;
+    }
+
+    // Give node access to neural network, set layer of node
+    public void Setup(int layer)
+    {
+        network = GameObject.FindGameObjectWithTag("Network").GetComponent<NeuralNetwork>();
+        this.layer = layer;
     }
 
     // Using the established connections and weights, calculate the output
-    public void calculateOutput()
+    public void CalculateOutput()
     {
         // If invalid weights and connections, print error and return
-        if(!validConnectionsAndWeights())
+        if(!ValidConnectionsAndWeights())
         {
             Debug.Log("Error in Neural Network: Number of weights does not match number of connections");
             return;
         }
 
+        // If this is an input layer node, we just set output equal to input
+        if(layer == 0)
+        {
+            output = input;
+            return;
+        }
 
+        // Standard output calculation
+        CalcOutput();
     }
 
     // Check to make sure there are the same amount of connections as weights
-    private bool validConnectionsAndWeights()
+    private bool ValidConnectionsAndWeights()
     {
         if(weights.Length == connections.Length)
         {
@@ -56,6 +73,26 @@ public class AINode : MonoBehaviour
         else
         {
             return false;
+        }
+    }
+
+    // Inner workings of CalculateOutput
+    // Gets output from connections and multiplies them by the weight for that connection
+    private void CalcOutput()
+    {
+        foreach(int connect in connections)
+        {
+            if(layer == 1)
+            {
+                float val = network.inputLayer[connect].GetOutput();
+                output =  val * weights[connect];
+            }
+
+            else
+            {
+                float val = network.hiddenLayer[connect].GetOutput();
+                output = val * weights[connect];
+            }
         }
     }
 }
--- GameFiles/Assets/Scripts/NeuralNetwork.cs
+++ GameFiles/Assets/Scripts/NeuralNetwork.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class NeuralNetwork : MonoBehaviour
 {
+    // Array of input layer nodes
+    public AINode[] inputLayer;
+
+    // Array of hidden layer nodes
+    public AINode[] hiddenLayer;
+
+    // Array of output layer nodes;
+    public AINode[] outputLayer;
+
+
     // Start is called before the first frame update
     void Start()
     {
