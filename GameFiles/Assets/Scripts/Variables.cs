0dda58e79613259095cfbc960c3460bfc4dc141f
LifeAlertt <60244208+LifeAlertt@users.noreply.github.com>
2020-03-29 15:51:58 -0400

    Added Variables script
    Varaibles inside this script are preserved upon scene change.

---
+++ GameFiles/Assets/Scripts/Variables.cs
@@ -0,0 +1,17 @@
+﻿// Store variables that persist through scenes
+public static class Variables
+{
+    private static int gameLevel;
+
+    public static int GameLevel
+    {
+        get
+        {
+            return gameLevel;
+        }
+        set
+        {
+            gameLevel = value;
+        }
+    }
+}
