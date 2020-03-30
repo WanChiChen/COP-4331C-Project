4540e54dce65e230b179eb88ad4e612a866b0e95
WaffleFisher <yale3312@gmail.com>
2020-03-30 01:45:58 -0400

    Prefabs and scripts
    Enemy prefabs and scripts, sending in before they fully work to make sure what ever happend in last two commits doesnt happen again.

---
+++ GameFiles/Assets/Scripts/EnemyFunctionality/EnemyMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyMovement : MonoBehaviour
+{
+    public float speed;
+    public float detectDist;
+    protected Transform player;
+
+    // Start is called before the first frame update
+    public virtual void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+}
