f99a5ec2e5fe16ada00f773dd08c3bef9142951c
wc6255 <wchen6255@outlook.com>
2020-03-30 01:28:22 -0400

    added ability UI

---
+++ GameFiles/Assets/Scripts/UIAbilityBar.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIAbilityBar : MonoBehaviour
+{
+    GameObject player;
+    PlayerAbilities abilities;
+    public List<UIAbility> uiAbilities = new List<UIAbility>();
+    public GameObject slotPrefab;
+    public Transform slotPanel;
+
+    const int numAbilities = 5;
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        abilities = player.GetComponent<PlayerAbilities>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject instance = Instantiate(slotPrefab);
+            instance.transform.SetParent(slotPanel);
+            uiAbilities.Add(instance.GetComponentInChildren<UIAbility>());
+
+            uiAbilities[i].index = i;
+            uiAbilities[i].showAbility(abilities.abilities[0]);
+            Debug.Log("dddd" + i);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
