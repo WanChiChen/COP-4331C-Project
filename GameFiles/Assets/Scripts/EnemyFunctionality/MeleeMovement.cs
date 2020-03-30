1e768ea12d98c22aa4ebabd091256c4804f2e6af
WaffleFisher <yale3312@gmail.com>
2020-03-30 00:40:42 -0400

    Scripts
    Enemy Scripts

---
+++ GameFiles/Assets/Scripts/EnemyFunctionality/MeleeMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeMovement : EnemyMovement
+{
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //checks distace from player, when close begins to move towards.
+        //Debug.Log((target.transform.position - this.transform.position).sqrMagnitude);
+        if ((player.transform.position - this.transform.position).sqrMagnitude < detectDist)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+        if ((player.transform.position - this.transform.position).sqrMagnitude < .08)
+        {
+            //attack
+        }
+
+    }
+}
