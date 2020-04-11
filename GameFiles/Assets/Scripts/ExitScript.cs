1ca64ec88837d8738f92b089c4ba56ffe0d82ccc
LifeAlertt <60244208+LifeAlertt@users.noreply.github.com>
2020-04-11 18:47:56 -0400

    Added boss spawning

--- GameFiles/Assets/Scripts/ExitScript.cs
+++ GameFiles/Assets/Scripts/ExitScript.cs
@@ -7,10 +7,34 @@ public class ExitScript : MonoBehaviour
 {
     PlayerProgressTracker progress;
 
+    // Player object
+    private GameObject player;
+
+    // Boss object
+    public GameObject boss;
+
+    private bool spawned = false;
+
     private void Awake()
     {
         progress = GameObject.Find("PlayerProgressTracker").GetComponent<PlayerProgressTracker>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
+
+    private void Update()
+    {
+        if(GetPlayerDistance() <= 8f && !spawned)
+        {
+            GameObject thisBoss = Instantiate(boss, transform.position + new Vector3(0, -1, 0), Quaternion.identity);
+            spawned = true;
+        }
+    }
+
+    private float GetPlayerDistance()
+    {
+        return Vector3.Distance(player.GetComponent<Transform>().position, transform.position);
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player")) // If object is player, generate new level
